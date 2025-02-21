909021232bc7313161eacfe6bfe9919145330b99
Artemis <clare.stock@gmail.com>
2025-02-20 20:29:16 -0500

    idk, poking at problems

--- recipeUI/src/app/components/recipes/recipes.component.ts
+++ recipeUI/src/app/components/recipes/recipes.component.ts
@@ -5,11 +5,12 @@ import { FullrecipeComponent } from '../fullrecipe/fullrecipe.component';
 import { IngredientComponent } from '../ingredient/ingredient.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RecipeDetailComponent } from "../recipe-detail/recipe-detail.component";
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [FullrecipeComponent, IngredientComponent, FormsModule, CommonModule],
+  imports: [FullrecipeComponent, IngredientComponent, FormsModule, CommonModule, RecipeDetailComponent],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css'
 })

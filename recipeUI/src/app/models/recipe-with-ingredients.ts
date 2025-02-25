428729649ea2a96e0c4d6fa99cf835549df6ef6a
Clare S <Clare.Stock@gmail.com>
2025-02-24 23:34:37 -0500

    class functions

--- recipeUI/src/app/models/recipe-with-ingredients.ts
+++ recipeUI/src/app/models/recipe-with-ingredients.ts
@@ -6,4 +6,28 @@ export class RecipeWithIngredients {
         public recipe: Recipe,
         public ingredients: Ingredients[]
     ){}
+
+    getPair(): [Recipe, Ingredients[]]{
+        return [this.recipe, this.ingredients];
+    }
+    
+    addToList(list:RecipeWithIngredients[]):void{
+        list.push(this);
+    }
+
+    static createPairsList(recipes: Recipe[], allIngredients: Ingredients[]):RecipeWithIngredients[]{
+        const pairsList: RecipeWithIngredients[] = [];
+
+        recipes.forEach(recipe => {
+            const matchingIngredients = allIngredients.filter(
+                ingredient => ingredient.recipe === recipe.title
+            );
+            const pair = new RecipeWithIngredients(recipe, matchingIngredients);
+            pairsList.push(pair);
+        });
+    
+    return pairsList;
+    }
+
 }
+
